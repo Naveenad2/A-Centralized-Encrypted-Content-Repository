5e50f5ef23e4d525a1c739f418c7ace12cb0bf82
Naveenad2 <naveenad260@gmail.com>
2023-08-15 22:34:31 +0530

    first testing commit

---
+++ A-Centralized-Encrypted-Content-Repository/src/main/java/com/Centralize/EncryptedContentRepository/A/Centralized/Encrypted/Content/Repository/ACentralizedEncryptedContentRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.Centralize.EncryptedContentRepository.A.Centralized.Encrypted.Content.Repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ACentralizedEncryptedContentRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ACentralizedEncryptedContentRepositoryApplication.class, args);
+	}
+
+}
---
+++ A-Centralized-Encrypted-Content-Repository/src/test/java/com/Centralize/EncryptedContentRepository/A/Centralized/Encrypted/Content/Repository/ACentralizedEncryptedContentRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Centralize.EncryptedContentRepository.A.Centralized.Encrypted.Content.Repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ACentralizedEncryptedContentRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
